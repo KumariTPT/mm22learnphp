f5baa7888155e1f00a36c00414a0618964581589
KumariTPT <kristofer.kumari@tptlive.ee>
2024-09-05 10:28:16 +0300

    push

--- index.php
+++ index.php
@@ -1,61 +1,7 @@
 <?php
 
-$num = 12; ///num on nimetus///
-
-$num = "hello";
-
-$num = "öö";
-
-$num = null;
-
-$a = 1;
-$a++;
-$a--;
-$a+=2;
-$a*=2;
-$a/=2;
-$a%=2;
-
-$a = 'hello ' . 'world';
-$a .='asdaskdj';
-$array = array(1,2,3); ///vana viis///
-$array = [1,2,3]; ///uus viis///
-
-var_dump($array[1]);
-$array =[
-    'essa' => 1,
-    'tessa' =>2,
-    'kossa' =>3,
-    5 => 4,
-    'asdasd',
-     10 => 1.2,
-    true
-];
-var_dump($array['kossa']);
-
+$array = [1,2,3,4];
+array_push($array,5,6,7);
+$array[] = 8;
+$array[4] = 123123;
 var_dump($array);
-foreach($array as $key => $value){
-    //echo $key . '=>' . $value . "\n";
-    echo "$key=>$value\n";
-}
-
-$name = 'Kristofer';
-
-$string = "hello $name\n";
-var_dump($string);
-$char = 'a';
-
-for($i=0;$i<100;$i++){
-    var_dump($char);
-    $char++;
-}
-
-
-
-echo 'hello world'; ///pmst print///
-
-var_dump($a);
-
-var_dump($num); ///var_dump trykib valja///
-
-?>
