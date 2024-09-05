40f2c7bd12a0cc9f4764522325bfcf333f018b49
KumariTPT <kristofer.kumari@tptlive.ee>
2024-09-05 11:16:18 +0300

    kastid

--- index.php
+++ index.php
@@ -1,13 +1,47 @@
 <?php
 
-$array = [1,2,3,4];
-array_push($array,5,6,7);
-$array[] = 8;
-$array[4] = 123123;
-var_dump($array);
+class Box {
+    public $height;
+    public $width;
+    public $length;
 
-function hello(){
-    echo "hello $name\n"; ///nimi///
+    public function volume(){
+        return $this->height * $this->width * $this->length;
+    }
 }
 
-hello(kristofer);
+class MetalBox {
+    public $weightPerUnit = 10;
+    public function weight(){
+        return $this->volume() * $this->weightPerUnit;
+    }
+}
+
+$num1 = 1;
+$num2 = $num1;
+$num2 = 2;
+var_dump($num1, $num2);
+
+$metal = new MetalBox ();
+$metal->height = 1;
+$metal->width = 2;
+$metal->length = 3;
+var_dump($metal);
+var_dump($metal->volume());
+var_dump($metal->weight());
+
+$box1 = new Box();
+$box1->height = 1;
+$box1->width = 2;
+$box1->length = 3;
+var_dump($box1);
+
+$box2 = new Box();
+$box2->height = 4;
+$box2->width = 5;
+$box2->length = 6;
+var_dump($box1);
+var_dump($box2);
+
+var_dump($box1->volume());
+var_dump($box2->volume());
